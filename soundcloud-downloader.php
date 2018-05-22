0f40be5f1b234bc992a2d67bfa29cddbc8f32f61
Nima HeydariNasab <nima.heydari79@yahoo.com>
2018-05-22 11:42:19 +0000

    Apply fixes from StyleCI

--- soundcloud-downloader.php
+++ soundcloud-downloader.php
@@ -6,17 +6,21 @@
  * NimaH79.ir
  */
 
-function getSoundcloudDownloadLink($url) {
+function getSoundcloudDownloadLink($url)
+{
     $ch = curl_init('https://soundpump.net/download');
     curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, array('uri' => $url));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, ['uri' => $url]);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     $result = curl_exec($ch);
     curl_close($ch);
     $result = json_decode($result, true);
-    if(!empty($result['downloadUrl'])) return $result['downloadUrl'];
+    if (!empty($result['downloadUrl'])) {
+        return $result['downloadUrl'];
+    }
+
     return false;
 }
 
