799184dad386d1c32b06997bcdffe8ca0376ab36
Nima HeydariNasab <nima.heydari79@yahoo.com>
2019-07-09 16:28:03 +0430

    Change API

--- soundcloud-downloader.php
+++ soundcloud-downloader.php
@@ -6,22 +6,42 @@
  * NimaH79.ir
  */
 
-function getSoundcloudDownloadLink($url)
+class SoundCloud
 {
-    $ch = curl_init('https://soundpump.net/download');
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, ['uri' => $url]);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    $result = curl_exec($ch);
-    curl_close($ch);
-    $result = json_decode($result, true);
-    if (!empty($result['downloadUrl'])) {
-        return $result['downloadUrl'];
+
+    private static $domain = 'dlnowsoft.com';
+    private static $scheme = 'https://';
+
+    public function getDownloadURL($url)
+    {
+        $subdomain_and_id = self::getSubdomainAndId();
+        $data = self::curl_get_contents(self::$scheme.$subdomain_and_id['subdomain'].'.'.self::$domain.'/get-json.php?'.http_build_query(['url' => $url, 'id' => $subdomain_and_id['id']]));
+        if(strpos($data, 'Error') !== false) {
+            return $data;
+        }
+        $data = json_decode($data, true);
+        
+        return isset($data['url']) ? $data['url'] : false;
     }
 
-    return false;
-}
+    private static function getSubdomainAndId() {
+        $page = self::curl_get_contents(self::$scheme.'www.'.self::$domain.'/soundcloud-mp3');
+        preg_match('/(\w+)\.dlnowsoft\.com\/g/', $page, $subdomain);
+        preg_match('/"&id=","(.*?)"/', $page, $id);
 
-echo getSoundcloudDownloadLink('https://soundcloud.com/nimsaz/jang-by-shahnazi');
+        return ['subdomain' => $subdomain[1], 'id' => $id[1]];
+    }
+
+    private static function curl_get_contents($url, $parameters = null) {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $parameters);
+        $response = curl_exec($ch);
+        curl_close($ch);
+
+        return $response;
+    }
+
+}
