6f9264592a6f1568539cb765b9bff2ba751a2685
Nima HeydariNasab <nima.heydari79@yahoo.com>
2019-07-09 16:30:11 +0430

    Add example

---
+++ example.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+ * This file is an example of SoundCloud.com downloader
+ * By NimaH79
+ * NimaH79.ir
+ */
+
+require __DIR__.'/SoundCloud.php';
+
+echo SoundCloud::getDownloadURL('https://soundcloud.com/nimsaz/jang-by-shahnazi');
